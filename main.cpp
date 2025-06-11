9d40fee66fe6692d74332824ad11758814b175ec
yTekipS <rafalburek50@gmail.com>
2025-06-11 17:22:55 +0200

    Version 1 of Ping Pong Game

--- main.cpp
+++ main.cpp
@@ -1,49 +1,142 @@
 #include "raylib.h"
 
+class Ball
+{
+public:
+    float x, y, radius = 20;
+    int y_speed = 9, x_speed = 9;
+    void Draw()
+    {
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void Move()
+    {
+        x += x_speed;
+        y += y_speed;
+
+        if (x + radius >= GetScreenWidth() || x - radius <= 0)
+        {
+            x_speed *= -1;
+        }
+        if (y + radius >= GetScreenHeight() || y - radius <= 0)
+        {
+            y_speed *= -1;
+        }
+    }
+};
+Ball ball;
+class Player
+{
+public:
+    float x, y;
+    const int speed = 9;
+    const float width = 30.0f, height = 120.0f;
+
+    void Draw()
+    {
+        DrawRectangle(x, y, width, height, DARKBLUE);
+    }
+
+    void Limit()
+    {
+        if (y <= 0)
+        {
+            y = 0;
+        }
+        if (y + height >= GetScreenHeight())
+        {
+            y = GetScreenHeight() - height;
+        }
+    }
+
+    void Move()
+    {
+        if (IsKeyDown(KEY_W))
+        {
+            y -= speed;
+        }
+        else if (IsKeyDown(KEY_S))
+        {
+            y += speed;
+        }
+        Limit();
+    }
+};
+
+Player player1;
+
+class Player2 : public Player
+{
+public:
+    void Draw()
+    {
+        DrawRectangle(x, y, width, height, DARKPURPLE);
+    }
+
+    void Move()
+    {
+        if (IsKeyDown(KEY_UP))
+        {
+            y -= speed;
+        }
+        else if (IsKeyDown(KEY_DOWN))
+        {
+            y += speed;
+        }
+        Limit();
+    }
+};
+
+Player2 player2;
+
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "Simple Raylib Temple.");
+    InitWindow(screenWidth, screenHeight, "Ping Pong Game");
 
-    Rectangle player = {400.0f, 285.0f, 50.0f, 50.0f};
+    ball.x = screenWidth / 2;
+    ball.y = screenHeight / 2;
 
-    Camera2D camera = {0};
-    camera.target = (Vector2){player.x + 20.0f, player.y + 20.0f};
-    camera.offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
-    camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
+    player1.x = 0;
+    player1.y = screenHeight / 2 - player1.height / 2;
 
+    player2.x = screenWidth - (player2.width * 1.5f);
+    player2.y = screenHeight / 2 - player2.height / 2;
     SetTargetFPS(60);
 
     //* MAIN GAME LOOP
     while (!WindowShouldClose())
     {
-        //* UPDATEa
+        //* UPDATE
         //----------------------------------------------------------------------------------
         // TODO: Update your variables here
-        if (IsKeyDown(KEY_W))
-            player.y -= 2;
-        else if (IsKeyDown(KEY_S))
-            player.y += 2;
-        if (IsKeyDown(KEY_A))
-            player.x -= 2;
-        else if (IsKeyDown(KEY_D))
-            player.x += 2;
+        ball.Move();
+        player1.Move();
+        player2.Move();
 
-        camera.target = (Vector2){player.x + 20.0f, player.y + 20.0f};
+        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player1.x, player1.y, player1.width, player1.height}))
+        {
+            ball.x_speed *= -1;
+        }
+        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player2.x, player2.y, player2.width, player2.height}))
+        {
+            ball.x_speed *= -1;
+        }
         //----------------------------------------------------------------------------------
 
         //* DRAW
         //----------------------------------------------------------------------------------
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        BeginMode2D(camera);
-            DrawCircle(400.0f, 285.0f, 40.0f, RED);
-            DrawRectangleRec(player, BLUE);
-        EndMode2D();
-
+        {
+            ClearBackground(BLACK);
+            ball.Draw();
+            player1.Draw();
+            player2.Draw();
+            DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, GRAY);
+        }
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
